f41ef442b0ee8202a2d5702d6f9b8b53f5308974
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-27 07:59:31 +0100

    Modified template-slider.php template part

--- templates/template-slider.php
+++ templates/template-slider.php
@@ -1,1 +1,12 @@
-Hello World...
+<section class="fullscreen">
+    <div class="fullscreen-child">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <h1><?= $instance['title']; ?></h1>
+                    <summary><?= $instance['footnote']; ?></summary>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
