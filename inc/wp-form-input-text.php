52dc2dc1194c5cf7f378a0136bd6bb778065e56a
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-27 07:58:40 +0100

    Modified shorthand echo statement

--- inc/wp-form-input-text.php
+++ inc/wp-form-input-text.php
@@ -1,4 +1,4 @@
 <p>
-    <label for="<?php echo $this->widgetObject->get_field_id($value); ?>"><?php _e($label) ?></label>
-    <input type="text" class="widefat" id="<?php echo $this->widgetObject->get_field_id($value); ?>" name="<?php echo $this->widgetObject->get_field_name($value); ?>" value="<?php echo $this->wpInstance[$value]; ?>" />
+    <label for="<?php echo $this->widgetObject->get_field_id($key); ?>"><?php _e($label) ?></label>
+    <input type="text" class="widefat" id="<?php echo $this->widgetObject->get_field_id($key); ?>" name="<?php echo $this->widgetObject->get_field_name($key); ?>" value="<?php echo $this->wpInstance[$key]; ?>" />
 </p>
