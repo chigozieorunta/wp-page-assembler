c0f640a9ad59b496c7587b9432cd822d040e8636
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-28 10:29:24 +0100

    Modified template slider template

--- templates/template-slider.php
+++ templates/template-slider.php
@@ -1,7 +1,7 @@
 <section class="pa-section pa-fullscreen" style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
     <div class="pa-fullscreen-child" style="background-color: rgba(0,0,0,0.5);">
         <div class="container-fluid">
-            <div class="row">
+            <div class="row" style="text-align: <?= $instance['textAlign']; ?>">
                 <div class="col-sm-10 col-sm-offset-1">
                     <h1 class="pa-caption" style="color: <?= $instance['captionColor']; ?>">
                         <?= $instance['title']; ?>
