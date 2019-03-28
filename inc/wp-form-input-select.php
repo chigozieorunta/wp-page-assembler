8e402f2a3ab6aeb1353288984f0c66265d1a1a90
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-28 10:27:07 +0100

    Resolved key index conflict on select input

--- inc/wp-form-input-select.php
+++ inc/wp-form-input-select.php
@@ -2,9 +2,9 @@
     <label for="<?php echo $this->widgetObject->get_field_id($key); ?>"><?php _e($label) ?></label>
     <select class="widefat" id="<?php echo $this->widgetObject->get_field_id($key); ?>" name="<?php echo $this->widgetObject->get_field_name($key); ?>">
         <?php foreach($value as $column=>$item): ?>
-        <?php $selected = (($this->wpInstance['key'] == $item) ? 'selected' : ''); ?>
+        <?php $selected = (($this->wpInstance[$key] == $item) ? 'selected' : ''); ?>
         <option value="<?= $item ?>" <?= $selected; ?>>
-            <?= $selected.$column; ?>
+            <?= $column; ?>
         </option>
         <?php endforeach; ?>
     </select>
