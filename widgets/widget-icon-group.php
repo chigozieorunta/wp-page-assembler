6210933991d58a6fed05a640ccb16fa4d725e7b0
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-10 15:32:19 +0100

    Added new widget icon group

---
+++ widgets/widget-icon-group.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * Filename:      widget-icon-group.php
+ * Plugin Name:   wp-page-assembler
+ * Plugin URI:    https://github.com/chigozieorunta/wp-page-assembler
+ * Description:   The Page Assember WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web pages easily
+ * Domain Path:   ./widgets
+ * Description:   The pa_slider class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/03/19
+*/
+
+add_action('widgets_init', create_function('', 'return register_widget("pa_icon_group");'));
+
+class pa_icon_group extends WP_Widget {
+    /**
+     * WordPress Widget Constructor Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function __construct() {
+		parent::__construct(
+			'pa_slider',
+			__('Page Assembler Icon Group', 'widget_name'),
+			array('description' => __('Display Icon Group...'))
+		);
+	}
+    
+    /**
+     * WordPress Widget Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function widget($args, $instance) {
+        extract($args);
+        require_once(WPPAGEASSEMBLER.'templates/template-icon-group.php');
+    }
+
+	/**
+     * WordPress Update Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        foreach($new_instance as $key=>$value) {
+            $instance[$key] = strip_tags($value);
+        }
+        return $instance;
+    }
+    
+    /**
+     * WordPress Form Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function form($instance) {
+		$defaults = array('title' => '');
+        $instance = wp_parse_args((array) $instance, $defaults);
+        $textAlign = array(
+            'left'      => 'left',
+            'center'    => 'center',
+            'right'     => 'right',
+            'justify'   => 'justify'
+        );
+        $sliderControls = array(
+            "title"             => "text",
+            "footnote"          => "text",
+            "revolutionSlider"  => "text",
+            "captionColor"      => "text",
+            "textColor"         => "text",
+            "backgroundColor"   => "text",
+            "backgroundImage"   => "text",
+            "backgroundOverlay" => "text",
+            "textAlign"         => $textAlign
+        );
+        $sliderControl = new wpFormControl($this, $instance);
+        foreach($sliderControls as $key=>$value) {
+            $sliderControl->getControl($key, $value);
+        }
+	}
+}
+
+?>
