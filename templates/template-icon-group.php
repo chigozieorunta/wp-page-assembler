2851771426a08e8f6b7b63855e26f853376e4246
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-15 12:53:14 +0100

    Modified template icon group file to contain page button and others

--- templates/template-icon-group.php
+++ templates/template-icon-group.php
@@ -1,58 +1,36 @@
 
 <!--Icon Group-->
 <section class="pa-section pa-section-md" style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
+    <?php if($instance['backgroundOverlay']): ?>
+        <div class="pa-overlay" style="background: <?= $instance['backgroundOverlay']; ?>"></div>
+    <?php endif; ?>
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-10 col-sm-offset-1">
+                <?php require('template-section-header.php'); ?>
                 <?php
-                /**
-                * Template Section Header;
-                */
+                    foreach($posts as $post): setup_postdata($post);
+                    $icon = get_post_meta($post->ID, 'icon', true);
                 ?>
-
-                <?php require_once('template-section-header.php'); ?>
-
-                <?php
-                /**
-                * Top Icon Template;
-                */
-                ?>
-
+                <?php if(!($currentPointer%$numberOfColumns)): ?>
+                <div class="row <?php if(($currentRow != $numberOfRows) || $instance['callToActionButtonText']): ?>row-spacer <?php endif; ?>row-col-spacer">
+                <?php endif; ?>
                 <?php if($instance['iconPosition'] == 'top'): ?>
-                <?php
-                    foreach($posts as $post): setup_postdata($post);
-                    $icon = get_post_meta($post->ID, 'icon', true);
-                ?>
-                <?php if(!($currentPointer%$numberOfColumns)): ?>
-                <div class="row <?php if($currentRow != $numberOfRows): ?>row-spacer <?php endif; ?>row-col-spacer">
-                <?php endif; ?>
                     <div class="<?= $columnClass; ?> text-center">
-                        <?php if($icon): ?><i class="<?php echo $icon; ?>"></i><?php endif; ?>
+                        <?php if($icon): ?>
+                            <i class="<?php echo $icon; ?>" style="margin-bottom: 15px;"></i>
+                        <?php endif; ?>
                         <h4 class="pa-caption" style="color: <?= $instance['captionColor']; ?>"><?php the_title(); ?></h4>
-                        <?php the_content(); ?>
+                        <div style="margin-top: -7.5px;">
+                            <?php if($instance['lengthOfPostText']): ?>
+                                <?= getShortenedText(get_the_excerpt(), $instance['lengthOfPostText']) ?>
+                            <?php else: ?>
+                                <?= get_the_excerpt(); ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                <?php $currentPointer++; ?>
-                <?php if(!($currentPointer%$numberOfColumns) || ($currentPointer == count($posts))): ?>
-                <?php $currentRow++; ?>
-                </div>
                 <?php endif; ?>
-                <?php endforeach; wp_reset_postdata(); ?>
-                <?php endif; ?>
-
-                <?php
-                /**
-                * Side Icon Template;
-                */
-                ?>
-                
                 <?php if($instance['iconPosition'] == 'side'): ?>
-                <?php
-                    foreach($posts as $post): setup_postdata($post);
-                    $icon = get_post_meta($post->ID, 'icon', true);
-                ?>
-                <?php if(!($currentPointer%$numberOfColumns)): ?>
-                <div class="row <?php if($currentRow != $numberOfRows): ?>row-spacer <?php endif; ?>row-col-spacer">
-                <?php endif; ?>
                     <div class="<?= $columnClass; ?>">
                         <div class="row">
                             <div class="col-xs-2 text-right">
@@ -60,18 +38,30 @@
                             </div>
                             <div class="col-xs-10">
                                 <h4 class="pa-caption" style="color: <?= $instance['captionColor']; ?>"><?php the_title(); ?></h4>
-                                <?php the_content(); ?>
+                                <?php if($instance['lengthOfPostText']): ?>
+                                    <?= getShortenedText(get_the_excerpt(), $instance['lengthOfPostText']) ?>
+                                <?php else: ?>
+                                    <?= get_the_excerpt(); ?>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
+                <?php endif; ?>
                 <?php $currentPointer++; ?>
                 <?php if(!($currentPointer%$numberOfColumns) || ($currentPointer == count($posts))): ?>
                 <?php $currentRow++; ?>
                 </div>
                 <?php endif; ?>
                 <?php endforeach; wp_reset_postdata(); ?>
-                <?php endif; ?>
 
+                <?php if($instance['callToActionButtonText']): ?>
+                <div class="row text-center">
+                    <div class="col-sm-12">
+                        <?php $link = get_page_link($instance['callToActionButtonPage']); ?>
+                        <a href="<?php echo $link; ?>" class="btn btn-primary <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
+                    </div>
+                </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
