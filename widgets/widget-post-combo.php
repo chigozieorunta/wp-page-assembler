18a1912d7e43ab251c94821a9b2246f7a0d2b293
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-18 20:55:06 +0100

    Added new widget for post combo

---
+++ widgets/widget-post-combo.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Filename:      widget-post-combo.php
+ * Plugin Name:   wp-page-assembler
+ * Plugin URI:    https://github.com/chigozieorunta/wp-page-assembler
+ * Description:   The Page Assember WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web pages easily
+ * Domain Path:   ./widgets
+ * Description:   The pa_post_combo class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/03/19
+*/
+
+add_action('widgets_init', create_function('', 'return register_widget("pa_post_combo");'));
+
+class pa_post_combo extends WP_Widget {
+    /**
+     * WordPress Widget Constructor Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function __construct() {
+		parent::__construct(
+			'pa_post_combo',
+			__('PA Post Combo', 'widget_name'),
+			array('description' => __('Display Post Combo...'))
+		);
+	}
+    
+    /**
+     * WordPress Widget Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function widget($args, $instance) {
+        extract($args);
+        $the_post = get_post($instance['post']);
+        $image = getImage($the_post);
+        $instance['height'] = ($instance['height']? $instance['height'] : 400);
+        require(WPPAGEASSEMBLER.'templates/template-post-combo.php');
+    }
+
+	/**
+     * WordPress Update Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        foreach($new_instance as $key=>$value) {
+            $instance[$key] = strip_tags($value);
+        }
+        return $instance;
+    }
+    
+    /**
+     * WordPress Form Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function form($instance) {
+		$defaults = array('title' => '');
+        $instance = wp_parse_args((array) $instance, $defaults);
+        global $wpPosts, $alternates, $wpPages;
+        $sliderControls = array(
+            "title"                     => "text",
+            "footnote"                  => "text",
+            "post"                      => $wpPosts,            
+            "alternate"                 => $alternates,
+            "height"                    => "text",
+            "captionColor"              => "text",
+            "textColor"                 => "text",
+            "backgroundColor"           => "text",
+            "backgroundImage"           => "text",
+            "backgroundOverlay"         => "text",
+            "callToActionButtonPage"    => $wpPages,
+            "callToActionButtonText"    => "text",
+            "callToActionButtonClass"   => "text"
+        );
+        $sliderControl = new wpFormControl($this, $instance);
+        foreach($sliderControls as $key=>$value) {
+            $sliderControl->getControl($key, $value);
+        }
+	}
+}
+
+?>
