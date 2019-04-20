c10307e237fb4db98fc36eba675a37a299e69592
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-20 12:09:13 +0100

    Added new contact form widget

---
+++ widgets/widget-contact-form.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * Filename:      widget-contact-form.php
+ * Plugin Name:   wp-page-assembler
+ * Plugin URI:    https://github.com/chigozieorunta/wp-page-assembler
+ * Description:   The Page Assember WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web pages easily
+ * Domain Path:   ./widgets
+ * Description:   The pa_contact_form class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/03/19
+*/
+
+add_action('widgets_init', create_function('', 'return register_widget("pa_contact_form");'));
+
+class pa_contact_form extends WP_Widget {
+    /**
+     * WordPress Widget Constructor Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function __construct() {
+		parent::__construct(
+			'pa_contact_form',
+			__('PA Contact Form', 'widget_name'),
+			array('description' => __('Display Contact Form...'))
+		);
+	}
+    
+    /**
+     * WordPress Widget Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function widget($args, $instance) {
+        extract($args);
+        require(WPPAGEASSEMBLER.'templates/template-contact-form.php');
+    }
+
+	/**
+     * WordPress Update Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        foreach($new_instance as $key=>$value) {
+            $instance[$key] = strip_tags($value);
+        }
+        return $instance;
+    }
+    
+    /**
+     * WordPress Form Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function form($instance) {
+		$defaults = array('title' => '');
+        $instance = wp_parse_args((array) $instance, $defaults);
+        global $wpPosts, $alternates, $wpPages, $textAlign;
+        $sliderControls = array(
+            "title"                     => "text",
+            "footnote"                  => "text",
+            "email"                     => "text",
+            "mailServer"                => "text",
+            "username"                  => "text",
+            "password"                  => "text",
+            "captionColor"              => "text",
+            "textColor"                 => "text",
+            "backgroundColor"           => "text",
+            "backgroundImage"           => "text",
+            "backgroundOverlay"         => "text",
+            "submitButtonText"          => "text",
+            "submitButtonClass"         => "text"
+        );
+        $sliderControl = new wpFormControl($this, $instance);
+        foreach($sliderControls as $key=>$value) {
+            $sliderControl->getControl($key, $value);
+        }
+	}
+}
+
+?>
