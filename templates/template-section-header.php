a2050ef665e8d2a1f7d75c0422954402ffb0727b
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-20 17:11:01 +0100

    Changed minor areas for spacing

--- templates/template-section-header.php
+++ templates/template-section-header.php
@@ -1,4 +1,4 @@
-<div class="row row-spacer text-center">
+<div class="row row-spacer-md text-center">
     <div class="col-sm-12">
         <h2 class="pa-caption" style="color: <?= $instance['captionColor']; ?>"><?= $instance['title']; ?></h2>
         <summary><?= $instance['footnote']; ?></summary>
