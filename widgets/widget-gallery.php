2588bd200bebb18a996211061a611718e75f0c27
chigozieorunta <chigozieorunta@yahoo.com>
2019-04-26 11:06:02 +0100

    Modified gallery widget

---
+++ widgets/widget-gallery.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Filename:      widget-gallery.php
+ * Plugin Name:   wp-page-assembler
+ * Plugin URI:    https://github.com/chigozieorunta/wp-page-assembler
+ * Description:   The Page Assember WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web pages easily
+ * Domain Path:   ./widgets
+ * Description:   The pa_gallery class
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/03/19
+*/
+
+add_action('widgets_init', create_function('', 'return register_widget("pa_gallery");'));
+
+class pa_gallery extends WP_Widget {
+    /**
+     * WordPress Widget Constructor Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function __construct() {
+		parent::__construct(
+			'pa_gallery',
+			__('PA Gallery', 'widget_name'),
+			array('description' => __('Display Gallery...'))
+		);
+	}
+    
+    /**
+     * WordPress Widget Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function widget($args, $instance) {
+        extract($args);
+        $instance['numberOfPosts'] = ($instance['numberOfPosts']? $instance['numberOfPosts'] : -1);
+        $columnClass = getColumnClass($instance['numberOfColumns']);
+        $posts = get_posts(
+            array(
+                'category_name' => sanitize_title($instance['category']),
+                'orderby'       => $instance['sortType'],
+                'order'         => $instance['sortOrder'],
+                'numberposts'   => $instance['numberOfPosts']
+            )
+        );
+        global $post; 
+        $currentPointer = 0; $currentRow = 1; 
+        $numberOfColumns = $instance['numberOfColumns'];
+        $numberOfRows = ceil(count($posts)/$numberOfColumns);
+        require(WPPAGEASSEMBLER.'templates/template-gallery.php');
+    }
+
+	/**
+     * WordPress Update Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        foreach($new_instance as $key=>$value) {
+            $instance[$key] = strip_tags($value);
+        }
+        return $instance;
+    }
+    
+    /**
+     * WordPress Form Method
+	 *
+     * @access public  
+	 * @since  1.0.0
+    */
+	function form($instance) {
+		$defaults = array('title' => '');
+        $instance = wp_parse_args((array) $instance, $defaults);
+        global $wpCategories, $wpPages, $imagePositions, $wpSortTypes, $wpSortOrders, $columns;
+        $sliderControls = array(
+            "title"                     => "text",
+            "footnote"                  => "text",
+            "category"                  => $wpCategories,            
+            "numberOfColumns"           => $columns,
+            "imagePosition"             => $imagePositions,
+            "numberOfPosts"             => "text",
+            "lengthOfPostText"          => "text",
+            "sortType"                  => $wpSortTypes,
+            "sortOrder"                 => $wpSortOrders,
+            "captionColor"              => "text",
+            "textColor"                 => "text",
+            "backgroundColor"           => "text",
+            "backgroundImage"           => "text",
+            "backgroundOverlay"         => "text",
+            "callToActionButtonPage"    => $wpPages,
+            "callToActionButtonText"    => "text",
+            "callToActionButtonClass"   => "text"
+        );
+        $sliderControl = new wpFormControl($this, $instance);
+        foreach($sliderControls as $key=>$value) {
+            $sliderControl->getControl($key, $value);
+        }
+	}
+}
+
+?>
