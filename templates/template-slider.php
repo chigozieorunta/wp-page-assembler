c47e97fa6481f3fff18b24ba7a6ba09c0b386ee4
chigozieorunta <chigozieorunta@yahoo.com>
2019-05-06 15:09:58 +0100

    Modified slider template to contain call to action page

--- templates/template-slider.php
+++ templates/template-slider.php
@@ -5,10 +5,23 @@
         <div class="container-fluid">
             <div class="row" style="text-align: <?= $instance['textAlign']; ?>">
                 <div class="col-sm-10 col-sm-offset-1">
-                    <h1 class="pa-caption" style="color: <?= $instance['captionColor']; ?>">
-                        <?= $instance['title']; ?>
-                    </h1>
-                    <summary><?= $instance['footnote']; ?></summary>
+                    <?php if($instance['title']): ?>
+                        <h1 class="pa-caption" style="color: <?= $instance['captionColor']; ?>">
+                            <?= $instance['title']; ?>
+                        </h1>
+                    <?php endif; ?>
+                    <?php if($instance['footnote']): ?>
+                        <summary><?= $instance['footnote']; ?></summary><br/>
+                    <?php endif; ?>
+                    
+                    <?php if($instance['callToActionButtonText']): ?>
+                    <div class="row text-center">
+                        <div class="col-sm-12">
+                            <?php $link = get_page_link($instance['callToActionButtonPage']); ?>
+                            <a href="<?= $link; ?>" class="btn btn-primary <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
+                        </div>
+                    </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
