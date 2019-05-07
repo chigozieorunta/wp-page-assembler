b041515e1ef32ac02fe8a403e1eac3131ae92dfb
chigozieorunta <chigozieorunta@yahoo.com>
2019-05-07 07:14:19 +0100

    Regularized header for plugin file

--- templates/template-slider.php
+++ templates/template-slider.php
@@ -1,21 +1,24 @@
 
 <!--Slider-->
 <section class="pa-section pa-fullscreen" style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
-    <div class="pa-fullscreen-child" style="background-color: rgba(0,0,0,0.5);">
+    <?php if($instance['backgroundOverlay']): ?>
+        <div class="pa-overlay" style="background: <?= $instance['backgroundOverlay']; ?>"></div>
+    <?php endif; ?>
+    <div class="pa-fullscreen-child">
         <div class="container-fluid">
             <div class="row" style="text-align: <?= $instance['textAlign']; ?>">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <?php if($instance['title']): ?>
+                <div class="col-sm-5 col-sm-offset-1">
+                    <?php if(trim($instance['title'])): ?>
                         <h1 class="pa-caption" style="color: <?= $instance['captionColor']; ?>">
                             <?= $instance['title']; ?>
                         </h1>
                     <?php endif; ?>
-                    <?php if($instance['footnote']): ?>
+                    <?php if(trim($instance['footnote'])): ?>
                         <summary><?= $instance['footnote']; ?></summary><br/>
                     <?php endif; ?>
                     
                     <?php if($instance['callToActionButtonText']): ?>
-                    <div class="row text-center">
+                    <div class="row">
                         <div class="col-sm-12">
                             <?php $link = get_page_link($instance['callToActionButtonPage']); ?>
                             <a href="<?= $link; ?>" class="btn btn-primary <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
