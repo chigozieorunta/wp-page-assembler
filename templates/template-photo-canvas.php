e92b4ce89d211148be66ee4e83224044e4c66970
chigozieorunta <chigozieorunta@yahoo.com>
2019-05-07 06:35:36 +0100

    New template for photo canvas created

---
+++ templates/template-photo-canvas.php
@@ -0,0 +1,38 @@
+
+<!--Photo Canvas-->
+<section style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
+    <?php if($instance['backgroundOverlay']): ?>
+        <div class="pa-overlay" style="background: <?= $instance['backgroundOverlay']; ?>"></div>
+    <?php endif; ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <?php
+                    foreach($posts as $post): setup_postdata($post);
+                    $image = getImage($post);
+                ?>
+                <?php if(!($currentPointer%$numberOfColumns)): ?>
+                <div class="row <?php if($instance['callToActionButtonText']): ?>row-spacer-lg <?php endif; ?>row-col-spacer">
+                <?php endif; ?>
+                <div class="<?= $columnClass; ?>" style="padding: 0 !important;">
+                    <img src="<?= $image; ?>" class="unim-square"/>
+                </div>
+                <?php $currentPointer++; ?>
+                <?php if(!($currentPointer%$numberOfColumns) || ($currentPointer == count($posts))): ?>
+                <?php $currentRow++; ?>
+                </div>
+                <?php endif; ?>
+                <?php endforeach; wp_reset_postdata(); ?>
+
+                <?php if($instance['callToActionButtonText']): ?>
+                <div class="row row-spacer-lg text-center">
+                    <div class="col-sm-12">
+                        <?php $link = get_page_link($instance['callToActionButtonPage']); ?>
+                        <a href="<?= $link; ?>" class="btn btn-primary <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
