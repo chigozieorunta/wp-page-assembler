e46b1210472069d388dd3cbe64cceb817797608c
chigozieorunta <chigozieorunta@yahoo.com>
2019-05-07 07:14:05 +0100

    Regularized header for plugin file

--- templates/template-section-header.php
+++ templates/template-section-header.php
@@ -1,6 +1,9 @@
-<div class="row row-spacer-md text-center">
+
+<?php if(trim($instance['title']) || trim($instance['footnote'])): ?>
+<div class="row row-spacer text-center">
     <div class="col-sm-12">
         <h2 class="pa-caption" style="color: <?= $instance['captionColor']; ?>"><?= $instance['title']; ?></h2>
         <summary><?= $instance['footnote']; ?></summary>
     </div>
 </div>
+<?php endif; ?>
