ad0834f8b4bae52910f238bcbd4ae5fb9ea6f668
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-01 12:14:50 +0100

    Made changes to references on pa

--- js/widgetify.js
+++ js/widgetify.js
@@ -1,20 +1,21 @@
 /**
- * pa-gallery - v1.0.0 - 2019-04-20
- * https://github.com/chigozieorunta/wp-page-assembler/js/pa-gallery.js
+ * widgetify.js - v1.0.0 - 2019-04-20
+ * https://github.com/chigozieorunta/wp-widgetify/js/widgetify.js
  *
  * Copyright (c) 2019 Chigozie Orunta
- * Licensed MIT <https://github.com/chigozieorunta/wp-page-assembler/blob/master/LICENSE>
+ * Licensed MIT <https://github.com/chigozieorunta/wp-widgetify/blob/master/LICENSE>
 **/
 
 (function($) {
 	var imgUrl = '';
 	$(document).ready(function() {
-		$(".pa-gallery").click(function() {
+		/* Gallery & SlideShow */
+		$(".widgetify-gallery").click(function() {
 			imgUrl = $(this).children(".unim-img").css("background-image");
-			$(".pa-slideshow").fadeIn("fast");
-			$(".pa-slideshow-image").css("background-image", imgUrl);
+			$(".widgetify-slideshow").fadeIn("fast");
+			$(".widgetify-slideshow-image").css("background-image", imgUrl);
 		})
-		$(".pa-slideshow").click(function() {
+		$(".widgetify-slideshow").click(function() {
 			$(this).fadeOut("fast");
 		})
 	});
