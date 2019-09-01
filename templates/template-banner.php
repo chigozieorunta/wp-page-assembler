a1545ee38b4b274b171b7cf43742b329986f5894
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-01 12:22:30 +0100

    Modified template-banner.php

--- templates/template-banner.php
+++ templates/template-banner.php
@@ -1,25 +1,25 @@
 
-<!--Banner-->
-<section class="pa-section pa-section-lg pa-banner" style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
+<!--Widgetify Banner-->
+<section class="widgetify-section widgetify-section-lg widgetify-banner" style="color: <?= $instance['textColor']; ?>; background-color: <?= $instance['backgroundColor']; ?>; background-image: url(<?= $instance['backgroundImage']; ?>);">
     <?php if($instance['backgroundOverlay']): ?>
-        <div class="pa-overlay" style="background: <?= $instance['backgroundOverlay']; ?>"></div>
+        <div class="widgetify-overlay" style="background: <?= $instance['backgroundOverlay']; ?>"></div>
     <?php endif; ?>
     <div class="container-fluid">
         <div class="row text-center">
             <div class="col-sm-8 col-sm-offset-2">
                 <?php if(trim($instance['title'])): ?>
-                    <h2 class="pa-caption" style="color: <?= $instance['captionColor']; ?>">
+                    <div class="widgetify-caption" style="color: <?= $instance['captionColor']; ?>">
                         <?= $instance['title']; ?>
-                    </h2>
+                    </div>
                 <?php endif; ?>
                 <?php if(trim($instance['footnote'])): ?>
-                    <summary class="pa-summary"><?= $instance['footnote']; ?></summary>
+                    <div class="widgetify-summary"><?= $instance['footnote']; ?></div>
                 <?php endif; ?>
                 <?php if($instance['callToActionButtonText']): ?>
                 <div class="row" style="margin-top: 1em;">
                     <div class="col-sm-12">
                         <?php $link = get_page_link($instance['callToActionButtonPage']); ?>
-                        <a href="<?php echo $link; ?>" class="btn btn-primary pa-anchor-btn <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
+                        <a href="<?php echo $link; ?>" class="btn btn-primary widgetify-anchor-btn <?= $instance['callToActionButtonClass']; ?>"><?= $instance['callToActionButtonText']; ?></a>
                     </div>
                 </div>
                 <?php endif; ?>
