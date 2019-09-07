ecef3190e56614758c51ac91c09116cc6b34827b
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-07 12:56:33 +0100

    Modified secition-header to contain wow animation

--- templates/template-section-header.php
+++ templates/template-section-header.php
@@ -1,14 +1,14 @@
 <!-- Header (Caption & Summary) -->
 <?php if(trim($instance['title']) || trim($instance['footnote'])): ?>
 <div class="row row-spacer text-center">
-    <div class="col-sm-12">
+    <div class="col-sm-10 col-sm-offset-1">
         <!--Caption-->
         <?php if(trim($instance['title'])): ?>
-            <div class="widgetify-caption" style="color: <?= $instance['captionColor']; ?>"><?= $instance['title']; ?></div>
+            <div class="widgetify-caption wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="0" style="color: <?= $instance['captionColor']; ?>"><?= $instance['title']; ?></div>
         <?php endif; ?>
         <!--Summary-->
         <?php if(trim($instance['footnote'])): ?>
-            <div class="widgetify-summary" style="color: <?= $instance['summaryColor']; ?>"><?= $instance['footnote']; ?></div>
+            <div class="widgetify-summary wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="300ms" style="color: <?= $instance['summaryColor']; ?>"><?= $instance['footnote']; ?></div>
         <?php endif; ?>
     </div>
 </div>
