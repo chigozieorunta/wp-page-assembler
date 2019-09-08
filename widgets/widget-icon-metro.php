0ba9f1e25486ea8ea7cdd9fc93371e2347c9f58e
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-08 23:45:16 +0100

    Added new global variables widgetAnimation, widgetBoxType

--- widgets/widget-icon-metro.php
+++ widgets/widget-icon-metro.php
@@ -65,7 +65,7 @@ class widgetify_icon_metro extends WP_Widget {
 	function form($instance) {
 		$defaults = array('title' => '');
         $instance = wp_parse_args((array) $instance, $defaults);
-        global $wpCategories, $wpPages, $iconPositions, $wpSortTypes, $wpSortOrders, $columns;
+        global $wpCategories, $wpPages, $iconPositions, $wpSortTypes, $wpSortOrders, $columns, $widgetAnimation, $widgetBoxType;
         $widgetControls = array(
             "title"                     => "text",
             "footnote"                  => "text",
@@ -89,7 +89,8 @@ class widgetify_icon_metro extends WP_Widget {
             "widgetId"                  => "text",
             "widgetClass"               => "text",
             "widgetPadding"             => "text",
-            "widgetAnimation"           => "text"
+            "widgetAnimation"           => $widgetAnimation,
+            "widgetBoxType"             => $widgetBoxType
         );
         $widgetControl = new wpWidgetifyControl($this, $instance);
         foreach($widgetControls as $key=>$value) {
