2dce9fe1f4e21180a8fbd584d07a14b25651da0b
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-08 23:45:03 +0100

    Added new global variables widgetAnimation, widgetBoxType

--- widgets/widget-photo-canvas.php
+++ widgets/widget-photo-canvas.php
@@ -65,7 +65,7 @@ class widgetify_photo_canvas extends WP_Widget {
 	function form($instance) {
 		$defaults = array('title' => '');
         $instance = wp_parse_args((array) $instance, $defaults);
-        global $wpCategories, $wpPages, $wpSortTypes, $wpSortOrders, $columns, $alternates, $imageShapes;
+        global $wpCategories, $wpPages, $wpSortTypes, $wpSortOrders, $columns, $alternates, $imageShapes, $widgetAnimation, $widgetBoxType;
         $widgetControls = array(
             "title"                     => "text",
             "footnote"                  => "text",
@@ -87,7 +87,8 @@ class widgetify_photo_canvas extends WP_Widget {
             "widgetId"                  => "text",
             "widgetClass"               => "text",
             "widgetPadding"             => "text",
-            "widgetAnimation"           => "text"
+            "widgetAnimation"           => $widgetAnimation,
+            "widgetBoxType"             => $widgetBoxType
         );
         $widgetControl = new wpWidgetifyControl($this, $instance);
         foreach($widgetControls as $key=>$value) {
