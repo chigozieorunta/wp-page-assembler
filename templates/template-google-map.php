0c776402ebbc46c15cf2e138c10ced71625f02a9
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-11 09:44:55 +0100

    Added WOW animation

--- templates/template-google-map.php
+++ templates/template-google-map.php
@@ -1,6 +1,6 @@
 
 <!--Google Map-->
-<section class="widgetify-section widgetify-google-map <?= $instance['widgetClass']; ?>" style="background-color: #131313; <?php if($instance['widgetPadding']): ?>padding: <?= $instance['widgetPadding']; ?>px 0;<?php endif; ?>; height: <?= $instance['height']; ?>px" id="<?= $instance['widgetId']; ?>">
+<section class="widgetify-section widgetify-google-map wow <?= $instance['widgetAnimation']; ?> <?= $instance['widgetClass']; ?>" style="background-color: #131313; <?php if($instance['widgetPadding']): ?>padding: <?= $instance['widgetPadding']; ?>px 0;<?php endif; ?>; height: <?= $instance['height']; ?>px" id="<?= $instance['widgetId']; ?>">
     <div class="mapouter">
         <div class="gmap_canvas">
             <iframe width="100%" height="<?= $instance['height']; ?>" id="gmap_canvas" src="https://maps.google.com/maps?q=<?= $instance['location']; ?>&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
