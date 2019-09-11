b28f2faba99e87dd2774311bad7797df290d79c3
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-11 09:38:38 +0100

    Added new style definitions to google map

--- templates/template-google-map.php
+++ templates/template-google-map.php
@@ -1,11 +1,22 @@
 
 <!--Google Map-->
-<section class="theme-inverse widgetify-google-map" style="height: <?php echo $instance['height']; ?>px">
+<section class="widgetify-section widgetify-google-map <?= $instance['widgetClass']; ?>" style="background-color: #131313; <?php if($instance['widgetPadding']): ?>padding: <?= $instance['widgetPadding']; ?>px 0;<?php endif; ?>; height: <?= $instance['height']; ?>px" id="<?= $instance['widgetId']; ?>">
     <div class="mapouter">
         <div class="gmap_canvas">
-            <iframe width="100%" height="<?php echo $instance['height']; ?>" id="gmap_canvas" src="https://maps.google.com/maps?q=<?php echo $instance['location']; ?>&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <iframe width="100%" height="<?= $instance['height']; ?>" id="gmap_canvas" src="https://maps.google.com/maps?q=<?= $instance['location']; ?>&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
             <script src="http://www.embedgooglemap.net/mapscript.js"></script>
         </div>
-        <style>.mapouter{overflow:hidden;height:<?php echo $instance['height']; ?>px;width:100%;}.gmap_canvas {background:none!important;height:<?php echo $instance['height']; ?>px;width:100%;}</style>
+        <style>
+            .mapouter {
+                overflow: hidden;
+                height: <?= $instance['height']; ?>px;
+                width: 100%;
+            }
+            .gmap_canvas {
+                background: none !important;
+                height: <?= $instance['height']; ?>px; 
+                width: 100%;
+            }
+        </style>
     </div>
 </section>
