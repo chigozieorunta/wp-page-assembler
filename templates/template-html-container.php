43d7ac9fe64b46619b92d0a3d1193ef409583380
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-11 09:45:23 +0100

    Added WOW animation

--- templates/template-html-container.php
+++ templates/template-html-container.php
@@ -1,3 +1,5 @@
 
 <!--HTML Container-->
+<section class="widgetify-section widgetify-html-container wow <?= $instance['widgetAnimation']; ?> <?= $instance['widgetClass']; ?>" style="<?php if($instance['widgetPadding']): ?>padding: <?= $instance['widgetPadding']; ?>px 0;<?php endif; ?>" id="<?= $instance['widgetId']; ?>">
 <?= $instance['html']; ?>
+</section>
