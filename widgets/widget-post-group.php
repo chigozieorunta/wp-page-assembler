c208df04e78563a27fc069800b3ddaef000e27c0
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-12 11:37:05 +0100

    Added new color controls

--- widgets/widget-post-group.php
+++ widgets/widget-post-group.php
@@ -79,6 +79,8 @@ class widgetify_post_group extends WP_Widget {
             "sortType"                  => $wpSortTypes,
             "sortOrder"                 => $wpSortOrders,
             "captionColor"              => "text",
+            "summaryColor"              => "text",
+            "subCaptionColor"           => "text",
             "textColor"                 => "text",
             "backgroundColor"           => "text",
             "backgroundImage"           => "text",
