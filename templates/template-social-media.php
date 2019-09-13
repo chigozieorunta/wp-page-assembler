f0461d46b19b77585f4646c12481066f2aa678c6
chigozieorunta <chigozieorunta@yahoo.com>
2019-09-13 01:26:35 +0100

    Added WOW animation

--- templates/template-social-media.php
+++ templates/template-social-media.php
@@ -1,12 +1,12 @@
 
 <!--Social Media-->
-<section class="widgetify-social-media hidden-xs text-center">
+<section class="widgetify-section widgetify-social-media hidden-xs text-center <?= $instance['widgetClass']; ?>" id="<?= $instance['widgetId']; ?>">
     <div class="fullscreen">
         <div class="fullscreen-child">
             <div class="row">
                 <div class="col-sm-12">
                     <?php if($instance['facebookLink']): ?>
-                        <a target="_blank" href="<?= $instance['facebookLink']; ?>">
+                        <a target="_blank" href="<?= $instance['facebookLink']; ?>" class="wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="0ms">
                             <div class="widgetify-facebook">
                                 <div class="fullscreen">
                                     <div class="fullscreen-child">
@@ -17,7 +17,7 @@
                         </a>
                     <?php endif; ?>
                     <?php if($instance['twitterLink']): ?>
-                        <a target="_blank" href="<?= $instance['twitterLink']; ?>">
+                        <a target="_blank" href="<?= $instance['twitterLink']; ?>" class="wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="100ms">
                             <div class="widgetify-twitter">
                                 <div class="fullscreen">
                                     <div class="fullscreen-child">
@@ -28,7 +28,7 @@
                         </a>
                     <?php endif; ?>
                     <?php if($instance['instagramLink']): ?>
-                        <a target="_blank" href="<?= $instance['instagramLink']; ?>">
+                        <a target="_blank" href="<?= $instance['instagramLink']; ?>" class="wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="200ms">
                             <div class="widgetify-instagram">
                                 <div class="fullscreen">
                                     <div class="fullscreen-child">
@@ -39,7 +39,7 @@
                         </a>
                     <?php endif; ?>
                     <?php if($instance['youtubeLink']): ?>
-                        <a target="_blank" href="<?= $instance['youtubeLink']; ?>">
+                        <a target="_blank" href="<?= $instance['youtubeLink']; ?>" class="wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="300ms">
                             <div class="widgetify-youtube">
                                 <div class="fullscreen">
                                     <div class="fullscreen-child">
@@ -50,7 +50,7 @@
                         </a>
                     <?php endif; ?>
                     <?php if($instance['linkedinLink']): ?>
-                        <a target="_blank" href="<?= $instance['linkedinLink']; ?>">
+                        <a target="_blank" href="<?= $instance['linkedinLink']; ?>" class="wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="400ms">
                             <div class="widgetify-linkedin">
                                 <div class="fullscreen">
                                     <div class="fullscreen-child">
