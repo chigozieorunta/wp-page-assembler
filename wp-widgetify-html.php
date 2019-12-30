8a009d9cbe0e2e1815fdd2d43acf9d40cf265adc
chigozieorunta <chigozieorunta@yahoo.com>
2019-12-30 16:44:29 +0100

    Modified Plugins page

--- wp-widgetify-html.php
+++ wp-widgetify-html.php
@@ -1,4 +1,5 @@
 <div id="wpbody" role="main">
+	<style>a.clear{text-decoration: none;}</style>
     <div id="wpbody-content" aria-label="Main content" tabindex="0">
 		<div id="screen-meta" class="metabox-prefs">
             <div id="contextual-help-wrap" class="hidden no-sidebar" tabindex="-1" aria-label="Contextual Help Tab">
@@ -13,35 +14,82 @@
 		</div>
 		<div class="wrap about-wrap full-width-layout">
 		    <h1>Widgetify</h1>
-            <p class="about-text">The Widgetify WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web page easily via drag and drop.</p>
+            <p class="about-text">The Widgetify WordPress Plugin is a collection of WordPress widgets designed to help web designers build sections of their web page easily via widgets.</p>
             </p>
+			<p>Contributors: <a class="clear" href="https://github.com/chigozieorunta">Chigozie Orunta</a></p>
             <h2 class="nav-tab-wrapper wp-clearfix">
     			<a href="#" class="nav-tab nav-tab-active">Widgets</a>
-    			<a href="documentation.php" class="nav-tab">Documentation</a>
-    			<a href="faq.php" class="nav-tab">FAQs</a>
 		    </h2>
 		    <div class="about-wrap-content">
             	<div class="feature-section four-col">
             		<div class="col">
-            			<div class="freedoms-image"></div>
-            			<h3>widgetify-iconset</h3>
-            			<p>The iconset helps you create a group of icons categorized under one group.</p>
+            			<h3>widgetify-banner</h3>
+            			<p>The banner helps you create a banner with a background picture that cuts across the full width of the screen.</p>
             		</div>
             		<div class="col">
-            			<div class="freedoms-image"></div>
-            			<h3>widgetify-blogset</h3>
-            			<p>To study how the program works and change it to make it do what you wish.</p>
+            			<h3>widgetify-blog-group</h3>
+            			<p>The blog group helps you create a blog group in two variants (portrait & landscape).</p>
             		</div>
             		<div class="col">
-            			<div class="freedoms-image"></div>
-            			<h3>widgetify-gmaps</h3>
-            			<p>To redistribute.</p>
-            		</div>
-            		<div class="col">
-            			<div class="freedoms-image"></div>
             			<h3>widgetify-contact-form</h3>
-            			<p>To distribute copies of your modified versions to others.</p>
+            			<p>The contact form helps you create a basic contact form that submits to an email.</p>
             		</div>
+            		<div class="col">
+            			<h3>widgetify-content-box</h3>
+            			<p>The content box helps you create a content area with title, text and associated image.</p>
+            		</div>
+            	</div>
+				<div class="feature-section four-col">
+            		<div class="col">
+            			<h3>widgetify-gallery</h3>
+            			<p>The gallery helps you create a collection of pictures with a slideshow to help the user browse through.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-google-map</h3>
+            			<p>The google map helps you add the classic google map into your website easily by specifying an address.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-html-container</h3>
+            			<p>To HTML container helps you add raw HTML code into your page by adding its widget to your page.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-icon-group</h3>
+            			<p>The icon group helps you add a set of (font-awesome, themify) icon columns into your page.</p>
+            		</div>
+            	</div>
+				<div class="feature-section four-col">
+            		<div class="col">
+            			<h3>widgetify-icon-metro</h3>
+            			<p>The icon metro helps you add a set of icons belonging to the same category using a metro effect.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-photo-canvas</h3>
+            			<p>The photo canvas helps you add a group of pictures with equal heights all joined together like boxes.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-post-combo</h3>
+            			<p>The post combo helps you add a post combo in a box-like format that contains equal height on both sides.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-post-group</h3>
+            			<p>The post group helps you add a group of posts belonging to a category to your web page.</p>
+            		</div>
+            	</div>
+				<div class="feature-section four-col">
+            		<div class="col">
+            			<h3>widgetify-post-metro</h3>
+            			<p>The post metro helps you add a metro effect to a post group. Just simply add widget and specify category and you're good.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-slider</h3>
+            			<p>The slider helps you add a full screen slider to the top part of your page. It is very handy and efficient for a slider.</p>
+            		</div>
+            		<div class="col">
+            			<h3>widgetify-social-media</h3>
+            			<p>The social media widget helps you add a social media sticker (facebook, twitter, instagram...) to your web page.</p>
+            		</div>
+					<div class="col">
+					</div>
             	</div>
             </div>
         </div>
