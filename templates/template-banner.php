256e23a82f6743d8e41ea71781c483e7f5f0dab4
chigozieorunta <chigozieorunta@yahoo.com>
2020-02-20 19:19:14 +0100

    Added pre/post hooker functions to banner template

--- templates/template-banner.php
+++ templates/template-banner.php
@@ -7,11 +7,13 @@
     <div class="container-fluid">
         <div class="row text-center">
             <div class="col-sm-8 col-sm-offset-2">
+                <?= widgetify_before_banner_caption(); ?>
                 <?php if(trim($instance['title'])): ?>
                     <div class="widgetify-caption wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="0ms" style="color: <?= $instance['captionColor']; ?>">
                         <?= $instance['title']; ?>
                     </div>
                 <?php endif; ?>
+                <?= widgetify_after_banner_caption(); ?>
                 <?php if(trim($instance['footnote'])): ?>
                     <div class="widgetify-text wow <?= $instance['widgetAnimation']; ?>" data-wow-delay="100ms"><?= $instance['footnote']; ?></div>
                 <?php endif; ?>
